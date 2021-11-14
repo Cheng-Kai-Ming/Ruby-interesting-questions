4fcf78d48f20fa0492d9c08dd339fe27b140d3c6
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2021-11-14 21:41:14 +0800

    insertion improvement

--- insertion.rb
+++ insertion.rb
@@ -4,30 +4,45 @@ def insertion_sort(list)
     count = 0
     n = 1
 
-    while TRUE
-      if list[the_index] < list[the_index-n] and the_index != 0
+
+    while true
+ 
+      if list[the_index] < list[the_index-n] and the_index >= n
         n += 1
         count = count + 1
+  
       else
           break
       end
     end
 
     if count != 0
-      if index - count-1 >=0
-        list2 = list[0..index - count-1 ]
+      list3 = []
+      if index - count ==0
+        list3 << list[index]
+        list3 += list[0..index-1]
+        list3 += list[index+1..list.length-1]
+        list= list3
+        p "insertion_sort = #{list}"
       else
-        list2 = []
+        list3 += list[0..index - count-1]
+        list3 << list[index] 
+        if list3.length < list.length
+          list3 += list[index-count...index]
+          list3 += list[index+1..list.length-1]
+          list = list3
+        p "insertion_sort = #{list}"
+
+        end
       end
-    
-      list2 << list[index]
-      list = list -list2
-      list = list2 + list 
     end
   end
   list
+
 end
 
-p insertion_sort([5,5,6,7,10,1,3])
+
+
+p insertion_sort([2,5,1,4,8,6,5])
 p insertion_sort([8,7,6,5,4,3,2,1])
-p insertion_sort([2,5,1,4,8,6,7,3])
+p insertion_sort([3,3,2])
